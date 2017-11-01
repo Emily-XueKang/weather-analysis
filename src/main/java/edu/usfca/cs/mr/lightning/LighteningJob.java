19e43bdaa8b3527305c91c29e24e8612640d0e67
Emily-Xue Kang <xkang3@dons.usfca.edu>
2017-11-01 00:13:27 -0700

    add ligntening job

---
+++ src/main/java/edu/usfca/cs/mr/lightning/LighteningJob.java
@@ -0,0 +1,7 @@
+package edu.usfca.cs.mr.lightning;
+
+/**
+ * Created by xuekang on 11/1/17.
+ */
+public class LighteningJob {
+}
---
+++ src/main/java/edu/usfca/cs/mr/lightning/LighteningReducer.java
@@ -0,0 +1,7 @@
+package edu.usfca.cs.mr.lightning;
+
+/**
+ * Created by xuekang on 10/31/17.
+ */
+public class LighteningReducer {
+}
