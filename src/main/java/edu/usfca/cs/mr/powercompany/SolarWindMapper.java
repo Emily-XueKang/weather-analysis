3e90e9d12dd7255174ccc420b262289be0e911b3
Emily-Xue Kang <xkang3@dons.usfca.edu>
2017-11-08 11:57:50 -0800

    add land judgement

--- src/main/java/edu/usfca/cs/mr/powercompany/SolarWindMapper.java
+++ src/main/java/edu/usfca/cs/mr/powercompany/SolarWindMapper.java
@@ -8,6 +8,7 @@ import java.util.StringTokenizer;
 /**
  * Created by xuekang on 11/7/17.
  */
+//19.land_cover_land1_sea0_surface
 public class SolarWindMapper extends Mapper<LongWritable, Text, Text, SolarWind> {
     @Override
     protected void map(LongWritable key, Text value, Context context)
@@ -18,13 +19,17 @@ public class SolarWindMapper extends Mapper<LongWritable, Text, Text, SolarWind>
         String wind_gust = "";
         String cloud_cover = "";
         String geohash = "";
+        String land = "";
         while (itr.hasMoreTokens()) {
-            String item = itr.nextToken();
-            if (i == 1) geohash = item;
-            if (i == 15) wind_gust = item;
-            if (i == 16) cloud_cover = item;
+            String feature = itr.nextToken();
+            if (i == 1) geohash = feature;
+            if (i == 15) wind_gust = feature;
+            if (i == 16) cloud_cover = feature;
+            if(i==18) land = feature;
             i++;
         }
-        context.write(new Text(geohash.substring(0,5)), new SolarWind(new Text(wind_gust), new Text(cloud_cover), new Text(geohash)));
+        if(land == "1"){
+            context.write(new Text(geohash.substring(0,5)), new SolarWind(new Text(wind_gust), new Text(cloud_cover), new Text(geohash)));
+        }
     }
 }
