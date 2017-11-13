be3a2dfd10426a29469f7be45083e85719835f29
Emily-Xue Kang <xkang3@dons.usfca.edu>
2017-11-13 12:11:53 -0800

    add round to wind devide

--- src/main/java/edu/usfca/cs/mr/powercompany/SolarWindReducer.java
+++ src/main/java/edu/usfca/cs/mr/powercompany/SolarWindReducer.java
@@ -14,7 +14,6 @@ import java.math.RoundingMode;
 public class SolarWindReducer extends Reducer<Text, SolarWind, Text, SolarWind>{
     @Override
     protected void reduce(Text key, Iterable<SolarWind> values, Context context) throws IOException, InterruptedException {
-        //double totalWind = 0.0;
         BigDecimal totalWind = new BigDecimal(0.0);
         BigDecimal totalCloud = new BigDecimal(0.0);
         int count = 0;
@@ -23,13 +22,12 @@ public class SolarWindReducer extends Reducer<Text, SolarWind, Text, SolarWind>{
         for(SolarWind val : values){
             DoubleWritable cloud = val.getCloudcover();
             DoubleWritable wind = val.getWindgust();
-            //totalWind +=  wind.get();
             totalWind = totalWind.add(new BigDecimal(wind.get()));
             totalCloud = totalCloud.add(new BigDecimal(cloud.get()));
             count++;
         }
         averageCould = totalCloud.divide(new BigDecimal(count),2, RoundingMode.HALF_UP).doubleValue();
-        averageWindspeed = totalWind.divide(new BigDecimal(count)).doubleValue();
+        averageWindspeed = totalWind.divide(new BigDecimal(count),2, RoundingMode.HALF_UP).doubleValue();
         if (averageWindspeed>10 || averageCould<50) {
             context.write(key, new SolarWind(new DoubleWritable(averageWindspeed), new DoubleWritable(averageCould)));
         }
